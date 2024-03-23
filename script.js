f4972621627bd97782503af49b83073a6fd3ab82
Raziq Din <144256646+nidqija@users.noreply.github.com>
2024-03-23 17:33:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 let gettablenum = document.getElementById('getTableNum');
 let Num = document.getElementById('num');
 let tablenum = document.getElementById('tableNum');
+let getfood = document.getElementById('getFood');
+let Error = document.getElementById('error');
+let order = document.getElementById('order');
+
 
 gettablenum.addEventListener('click' , function(){
-    Num.innerHTML = tablenum.value;
-    tablenum.value = '';
+   if(tablenum.value == ''){
+      Error.innerHTML = 'Please Enter Table Number!';
+   } else{
+      Num.innerHTML = tablenum.value;
+      tablenum.value = '';
+      Error.innerHTML = '';
+   }
+   
 });
+
+
+
+getfood.addEventListener('click' , function(){
+   if (Num.innerHTML == '' ){
+      Error.innerHTML = 'You must fill your table no. first!';
+   } else{
+    let Dish = document.createElement('p');
+    Dish.classList.add('dish');
+    Error.innerHTML = '';
+    Dish.innerHTML = 'Buttermilk Chicken';
+    order.appendChild(Dish);
+   }
+});
+
+
+
+
+
+
 
 
