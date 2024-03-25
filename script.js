933e773597bb93b456f1827ca9be0fb2ab3da3fa
Raziq Din <144256646+nidqija@users.noreply.github.com>
2024-03-25 12:58:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ let gettablenum = document.getElementById('getTableNum');
 let Num = document.getElementById('num');
 let tablenum = document.getElementById('tableNum');
 let getfood = document.getElementById('getFood');
+let getfood2 = document.getElementById('getFood2');
 let Error = document.getElementById('error');
 let order = document.getElementById('order');
 let count = 0;
+let count2 = 0;
+let increment = 0;
+let increment2 = 0;
 
 
 gettablenum.addEventListener('click' , function(){
    if(tablenum.value == ''){
-      Error.innerHTML = 'Please Enter Table Number!';
+      Error.innerHTML = 'Please enter table number!';
    } else{
       Num.innerHTML = tablenum.value;
       tablenum.value = '';
@@ -26,15 +30,48 @@ getfood.addEventListener('click' , function(){
    } else{
     let Dish = document.getElementById('dish');
     let Amount = document.getElementById('amount');
+    let Price = document.getElementById('price');
+    
     count++;
+    increment+= 11;
     Amount.innerHTML = ' x ' + count;
     Amount.classList.add('amount');
     Dish.classList.add('dish');
+    Price.classList.add('price');
     Error.innerHTML = '';
     Dish.innerHTML = 'Buttermilk Chicken';
-    
+    Price.innerHTML = 'RM' + increment;
    }
 });
+
+
+getfood2.addEventListener('click' , function(){
+   if (Num.innerHTML == '' ){
+      Error.innerHTML = 'You must fill your table no. first!';
+   } else{
+    let Dish2 = document.getElementById('dish2');
+    let Amount2 = document.getElementById('amount2');
+    let Price2 = document.getElementById('price2');
+    
+    count2++;
+    increment2+= 12;
+    Amount2.innerHTML = ' x ' + count2;
+    Amount2.classList.add('amount');
+    Dish2.classList.add('dish');
+    Price2.classList.add('price');
+    Error.innerHTML = '';
+    Dish2.innerHTML = 'Chicken Lasagna';
+    Price2.innerHTML = 'RM' + increment2;
+   }
+});
+
+
+
+
+
+
+
+
 
 
 
